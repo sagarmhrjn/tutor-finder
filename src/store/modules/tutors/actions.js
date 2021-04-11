5c12096690409b7e27688500d221769d5be6292f
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-12 00:43:06 +0545

    sending a PUT http request to store tutor data

--- src/store/modules/tutors/actions.js
+++ src/store/modules/tutors/actions.js
@@ -1,13 +1,27 @@
 export default {
-    registerTutor(context, data) {
+    async registerTutor(context, data) {
+        const userId = context.rootGetters.userId
         const tutorData = {
-            id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             areas: data.areas,
             description: data.desc,
             hourlyRate: data.rate
         }
-        context.commit('registerTutor', tutorData)
+
+        const response = await fetch(`https://vue-http-demo-c43fa-default-rtdb.firebaseio.com/tutors/${userId}.json`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(tutorData)
+            })
+
+        if (!response.ok) {
+            // error todo
+        }
+
+        context.commit('registerTutor', {
+            ...tutorData,
+            id: userId
+        })
     }
 }
