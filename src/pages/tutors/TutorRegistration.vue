757773123741269be19177fe720f6995e82f8e50
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-11 16:54:16 +0545

    registering as the tutor form

--- src/pages/tutors/TutorRegistration.vue
+++ src/pages/tutors/TutorRegistration.vue
@@ -1,9 +1,17 @@
 <template>
-    <div></div>
+  <section>
+    <base-card>
+      <h2>Register as a tutor now!</h2>
+      <tutor-form></tutor-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import TutorForm from "../../components/tutors/TutorForm";
 export default {
-    
-}
+    components:{
+        TutorForm
+    }
+};
 </script>
