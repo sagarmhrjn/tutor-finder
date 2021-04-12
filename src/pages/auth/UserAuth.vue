8436a67e8a9540e13bd6045d393ff7ad3d6f6de2
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-12 20:49:31 +0545

    Better UX loading spinner and error handling

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,24 +1,33 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">E-Mail</label>
-        <input type="email" id="email" v-model="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="password" />
-      </div>
-      <p v-if="!formIsValid">
-        Please enter a valid email and password(must be atleast 6 characters
-        long)
-      </p>
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{
-        switchModeButtonCaption
-      }}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" title="An error occured" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" fixed>
+      <p>Authenticating...</p>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-Mail</label>
+          <input type="email" id="email" v-model="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password(must be atleast 6 characters
+          long)
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -29,6 +38,8 @@ export default {
       password: "",
       formIsValid: true,
       mode: "login",
+      isLoading: false,
+      error: null,
     };
   },
   computed: {
@@ -48,7 +59,7 @@ export default {
     },
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       if (
         this.email === "" ||
         !this.email.includes("@") ||
@@ -57,14 +68,23 @@ export default {
         this.formIsValid = false;
         return;
       }
-      if (this.mode === "login") {
-        // ...
-      } else {
-        this.$store.dispatch("signup", {
-          email: this.email,
-          password: this.password,
-        });
+
+      this.isLoading = true;
+
+      try {
+        if (this.mode === "login") {
+          // ...
+        } else {
+          await this.$store.dispatch("signup", {
+            email: this.email,
+            password: this.password,
+          });
+        }
+      } catch (err) {
+        this.error = err.message || "Failed to authenticate, try later.";
       }
+
+      this.isLoading = false;
     },
     switchAuthMode() {
       if (this.mode === "login") {
@@ -72,6 +92,9 @@ export default {
       } else {
         this.mode = "login";
       }
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
