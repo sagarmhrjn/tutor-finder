a2b09b71b7df796a4fd375658cb24d7c02814ef9
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-12 12:53:22 +0545

    added not found component

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-    <div></div>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check all our
+        <router-link to="/tutors">Tutors</router-link>.
+      </p>
+    </base-card>
+  </section>
 </template>
